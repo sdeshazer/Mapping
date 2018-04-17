22591929427d3b44ac9e5e22ef9ca90cf5b44459
CodingDevils <masterplayert@hotmail.com>
2018-04-16 17:08:43 -0700

    Nice working script yay
    Made it work nice

--- Calibration.cpp
+++ Calibration.cpp
@@ -1,3 +1,10 @@
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+
 //opencv2:
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
@@ -10,13 +17,6 @@
 #include <opencv/cv.h>
 //====================
 
-#include "stdafx.h"
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <fstream>
-
 // ==[namespace]==
 using namespace std;
 using namespace cv;
@@ -24,7 +24,10 @@ using namespace cv;
 // ================
 //       onMouse
 // ================
-int Id;
+string ComputerUserName = "Maste";
+vector<int> xValues;
+vector<int> yValues;
+
 void onMouse(int evt, int x, int y, int flags, void* param) {
 	if (evt == CV_EVENT_LBUTTONDOWN) {
 		std::vector<cv::Point>* ptPtr = (std::vector<cv::Point>*)param;
@@ -34,58 +37,75 @@ void onMouse(int evt, int x, int y, int flags, void* param) {
 // ================
 //     MouseEvent
 // ================
-void id(int i) {
-
-	cout << "ID: ";
-	cin >> i; 
-	cout << " "; 
-
+string ParkingLotName() {
+	xValues.clear();
+	yValues.clear();
+	string i;
+	cout << "Examples: " << endl;
+	cout << "This business has multiple parking lots so they labeled them as so: " << endl;
+	cout << "<Insert Business Name Here> Parking Lot 1" << endl;
+	cout << "<Insert Business Name Here> Parking Lot 2" << endl;
+	cout << "This business only had one parking lot so they labeled it as so: " << endl;
+	cout << "<Insert Business Name Here> Parking Lot" << endl;
+	cout << "Please type the Id of the lot: " << endl;
+	cout << "ParkingLotID: ";
+	cin >> i;
+	cout << endl;
+	return i;
 }
 
-void mouseEvent(int event, int x, int y, int flags, void* param){
+void mouseEvent(int event, int x, int y, int flags, void* param) {
 	IplImage* img = (IplImage*)param;
 
-	if (event == CV_EVENT_LBUTTONDOWN){
-	
+	if (event == CV_EVENT_LBUTTONDOWN) {
+
 		printf("%d %d\n",
 			x, y);
-		
+		xValues.push_back(x);
+		yValues.push_back(y);
 	}//if
-	ifstream f("C:\\Users\\Arg0s2\\Music\\newdata.txt");
-	ofstream a_file;
-	if (f.bad()) {
-		a_file.open("C:\\Users\\Arg0s2\\Music\\newdata.txt");
-	}
-	a_file << x << " " << y << endl;
 }//mousevent
 
-// ================
-//       MAIN
-// ================
+ // ================
+ //       MAIN
+ // ================
 
-int main(){
+int main() {
 	//int x, y;
-	id(Id);
-	
-	IplImage *img = cvLoadImage("C:\\Users\\Arg0s2\\Pictures\\apptest\\hsbmapping.png", 1); //load in parkinglot you wish to map
+	string nameOfParkingLot = ParkingLotName();
+	const char* ParkinglotToMapFile = string("C:\\Users\\" + ComputerUserName + "\\Pictures\\apptest\\hsbmapping.png").c_str();
+	IplImage *img = cvLoadImage(ParkinglotToMapFile, 1); //load in parkinglot you wish to map
 
-	Mat howtoVector = imread("C:\\Users\\Arg0s2\\Music\\howtomap.png", 0); 
+	Mat howtoVector = imread("C:\\Users\\" + ComputerUserName + "\\Music\\howtomap.png", 0);
 
 	cvNamedWindow("My Window", 1);
 	cvNamedWindow("How To Map Vector Coordinates", 1);
 	imshow("How To Map Vector Coordinates", howtoVector);
-	
+
 	cvSetMouseCallback("My Window", mouseEvent, &img);
 	cvShowImage("My Window", img);
 
 	cvWaitKey(0);
-	
-	cout << "writing txt file..." << endl;
-	//ofstream outFile("", ios::out);
-	cout << "text file complete." << endl;
 
-	ifstream b_file("C:\\Users\\Arg0s2\\Music\\newdata.txt"); 
+	cout << "Writing text file..." << endl;
+	ofstream a_file;
+	int currentSpot = 1;
+	a_file.open("C:\\Users\\" + ComputerUserName + "\\Music\\" + nameOfParkingLot + ".txt");
 
+	for (int i = 0; i < xValues.size(); i++) {
+		if (i == 0) {
+			a_file << "Parking Spot" << currentSpot << endl;
+			currentSpot++;
+		}
+
+		if (i % 4 == 0 && i != 0) {
+			a_file << "Parking Spot" << currentSpot << endl;
+			currentSpot++;
+		}
+		a_file << xValues[i] << " " << yValues[i] << endl;
+	}
+
+	cout << "Text file complete." << endl;
+	a_file.close();
 	return 0;
-
 }
